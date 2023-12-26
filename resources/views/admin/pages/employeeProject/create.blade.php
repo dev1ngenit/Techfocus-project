047a94e50e18f25907593a0688707524d5ac5f3b
Sazeduzzaman <sazeduzzaman@yahoo.com>
2023-12-26 12:09:45 +0600

    employee-projec Form Done

--- resources/views/admin/pages/employeeProject/create.blade.php
+++ resources/views/admin/pages/employeeProject/create.blade.php
@@ -14,7 +14,7 @@
                                 </div>
                             </div>
                             <div class="col-lg-6 col-sm-12 text-lg-end text-sm-center">
-                                <a href="{{ route('admin.company.index') }}"
+                                <a href="{{ route('admin.employee-project.index') }}"
                                     class="btn btn-icon btn-primary w-auto px-3 rounded-0">
                                     <i class="las la-arrow-left fs-2 me-2"></i> Back</a>
                             </div>
@@ -22,7 +22,7 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <form id="myForm" action="{{ route('admin.company.store') }}" method="post"
+                    <form id="myForm" action="{{ route('admin.employee-project.store') }}" method="post"
                         enctype="multipart/form-data">
                         @csrf
                         <div class="container px-0">
@@ -41,8 +41,7 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04"
-                                                class="form-label required mb-0">Type</label>
+                                            <label for="validationCustom04" class="form-label mb-0">Type</label>
                                             <select
                                                 class="form-select form-select-solid form-select-sm @error('country') is-invalid @enderror"
                                                 name="type[]" data-control="select2" data-filter="false"
@@ -60,7 +59,7 @@
 
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04" class="form-label required mb-0">Start
+                                            <label for="validationCustom04" class="form-label mb-0">Start
                                                 Date</label>
                                             <input type="date"
                                                 class="form-control form-control-solid form-control-sm @error('start_date') is-invalid @enderror"
@@ -71,7 +70,7 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04" class="form-label required mb-0">End
+                                            <label for="validationCustom04" class="form-label mb-0">End
                                                 Date</label>
                                             <input type="date"
                                                 class="form-control form-control-solid form-control-sm @error('end_date') is-invalid @enderror"
@@ -81,7 +80,7 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04" class="form-label required mb-0">Start
+                                            <label for="validationCustom04" class="form-label mb-0">Start
                                                 Time</label>
                                             <input type="time"
                                                 class="form-control form-control-solid form-control-sm @error('start_time') is-invalid @enderror"
@@ -95,7 +94,7 @@
 
 
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04" class="form-label required mb-0">End
+                                            <label for="validationCustom04" class="form-label mb-0">End
                                                 Time</label>
                                             <input type="time"
                                                 class="form-control form-control-solid form-control-sm @error('end_time') is-invalid @enderror"
@@ -105,8 +104,7 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04"
-                                                class="form-label required mb-0">Supervisor</label>
+                                            <label for="validationCustom04" class="form-label mb-0">Supervisor</label>
                                             <select class="form-select form-select-solid" name="supervisor" id="field2"
                                                 multiple multiselect-search="true" multiselect-select-all="true"
                                                 multiselect-max-items="3" onchange="console.log(this.selectedOptions)">
@@ -124,7 +122,7 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04" class="form-label required mb-0">Assigned
+                                            <label for="validationCustom04" class="form-label mb-0">Assigned
                                                 Employee</label>
                                             <select
                                                 class="form-select form-select-solid @error('assigned_employee') is-invalid @enderror"
@@ -140,13 +138,13 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04" class="form-label required mb-0">Project
+                                            <label for="validationCustom04" class="form-label mb-0">Project
                                                 Status</label>
                                             <select
                                                 class="form-select form-select-solid form-select-sm @error('project_status') is-invalid @enderror"
                                                 name="project_status" data-dropdown-parent="#brandAddModal"
                                                 data-control="select2" data-placeholder="Select an option"
-                                                data-allow-clear="true" required>
+                                                data-allow-clear="true">
                                                 <option value="planned">Planned</option>
                                                 <option value="on_going">On Going</option>
                                                 <option value="completed">Completed</option>
@@ -156,8 +154,7 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04"
-                                                class="form-label required mb-0">Review</label>
+                                            <label for="validationCustom04" class="form-label mb-0">Review</label>
                                             <textarea class="form-control form-control-solid form-control-sm"
                                                 name="review" id="" cols="30" rows="1"
                                                 placeholder="Enter Your Review"></textarea>
@@ -167,8 +164,7 @@
                                         </div>
 
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04"
-                                                class="form-label required mb-0">Status</label>
+                                            <label for="validationCustom04" class="form-label mb-0">Status</label>
                                             <input type="text"
                                                 class="form-control form-control-solid form-control-sm @error('end_time') is-invalid @enderror"
                                                 name="status" id="validationCustom01" placeholder="Enter Your Status">
@@ -177,8 +173,7 @@
                                             @enderror
                                         </div>
                                         <div class="col-md-3 mb-2">
-                                            <label for="validationCustom04"
-                                                class="form-label required mb-0">Weight</label>
+                                            <label for="validationCustom04" class="form-label mb-0">Weight</label>
                                             <input type="text"
                                                 class="form-control form-control-solid form-control-sm @error('weight') is-invalid @enderror"
                                                 name="weight" id="validationCustom01" placeholder="Enter Your Weight">
@@ -188,7 +183,7 @@
                                         </div>
 
                                         <div class="col-md-4 mb-2">
-                                            <label for="validationCustom04" class="form-label required mb-0">Kpi
+                                            <label for="validationCustom04" class="form-label mb-0">Kpi
                                                 Rating</label>
                                             <input type="number"
                                                 class="form-control form-control-solid form-control-sm @error('kpi_rating') is-invalid @enderror"
@@ -200,12 +195,12 @@
                                         </div>
                                         <div class="col-md-4 mb-2">
                                             <label for="validationCustom04"
-                                                class="form-label required mb-0 @error('total_working_day') is-invalid @enderror">Total
+                                                class="form-label mb-0 @error('total_working_day') is-invalid @enderror">Total
                                                 Working Day</label>
                                             <select class="form-select form-select-solid form-select-sm"
                                                 name="total_working_day" data-dropdown-parent="#brandAddModal"
                                                 data-control="select2" data-placeholder="Select an option"
-                                                data-allow-clear="true" required>
+                                                data-allow-clear="true">
                                                 <option value="1">Option 1</option>
                                                 <option value="2">Option 2</option>
                                             </select>
@@ -215,7 +210,7 @@
                                         </div>
                                         <div class="col-md-4 mb-2">
                                             <label for="validationCustom04"
-                                                class="form-label required mb-0 @error('total_working_day') is-invalid @enderror">Total
+                                                class="form-label mb-0 @error('total_working_day') is-invalid @enderror">Total
                                                 Working Man Hour</label>
                                             <input type="number"
                                                 class="form-control form-control-solid form-control-sm @error('total_working_man_hour') is-invalid @enderror"
@@ -227,7 +222,7 @@
                                         </div>
 
                                         <div class="col-md-12 mb-2">
-                                            <label for="longText" class="form-label required mb-0">Long Text</label>
+                                            <label for="longText" class="form-label mb-0">Long Text</label>
                                             <textarea id="longText" name="longText"
                                                 class="tox-target kt_docs_tinymce_plugins @error('longText') is-invalid @enderror">{{ old('longText') }} </textarea>
                                             @error('longText')
